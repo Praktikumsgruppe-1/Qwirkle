fcbb7ce69545be04c53838596d12f803b6bda94a
WariofeatBanana <58808147+WariofeatBanana@users.noreply.github.com>
2020-01-25 22:21:04 +0100

    die klammer muss weg

--- Project Q/regeln.cpp
+++ Project Q/regeln.cpp
@@ -268,7 +268,6 @@ bool Regeln::check2(int xCoord, int yCoord, int colour, int symbol)
         note=note+1;
         qDebug() << note;
       }
-        }
         while(feldarray[note][yCoord][1]>0);
         qDebug("solange nach rechts gehen wie steine liegen");
     }
