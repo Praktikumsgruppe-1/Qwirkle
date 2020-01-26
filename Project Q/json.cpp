dcbdbb63bc9821bd2f86583747ec4e6515633898
JeschkeP <58844029+JeschkeP@users.noreply.github.com>
2020-01-27 00:11:06 +0100

    vector-fkt to json.cpp

--- Project Q/json.cpp
+++ Project Q/json.cpp
@@ -1,6 +1,28 @@
 #include "json.h"
+#include <QJsonArray>
+#include <QJsonObject>
+#include <vector>
 
 json::json()
 {
 
+}
+QJsonArray json::toJson (const std::vector <int> vec){
+    QJsonArray erg;
+    for (auto i = vec.begin(); i!= vec.end(); i++) {
+        erg.push_back((*i));
+    }
+    return erg;
+}
+
+std::vector <int> toVector(QJsonArray ar) {
+    std::vector <int> vec;
+    int anz = ar.size();
+    int tmp;
+    //ganzes QJsonArray durchgehen
+    for (int i = 0; i < anz; i++) {
+        tmp = ar[i].toInt();
+        vec.push_back(tmp);
+    }
+    return vec;
 }
