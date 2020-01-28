86a477641a22617bfcf0cb3ebcbf4700fb8ee76d
WariofeatBanana <58808147+WariofeatBanana@users.noreply.github.com>
2020-01-28 12:18:02 +0100

    netzwerk zeugs

--- Project Q/debug/moc_game.cpp
+++ Project Q/debug/moc_game.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Game_t {
-    QByteArrayData data[20];
-    char stringdata0[296];
+    QByteArrayData data[22];
+    char stringdata0[316];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -45,13 +45,15 @@ QT_MOC_LITERAL(9, 157, 15), // "messageReceived"
 QT_MOC_LITERAL(10, 173, 6), // "sender"
 QT_MOC_LITERAL(11, 180, 4), // "text"
 QT_MOC_LITERAL(12, 185, 11), // "sendMessage"
-QT_MOC_LITERAL(13, 197, 22), // "disconnectedFromServer"
-QT_MOC_LITERAL(14, 220, 5), // "error"
-QT_MOC_LITERAL(15, 226, 28), // "QAbstractSocket::SocketError"
-QT_MOC_LITERAL(16, 255, 11), // "socketError"
-QT_MOC_LITERAL(17, 267, 10), // "userJoined"
-QT_MOC_LITERAL(18, 278, 8), // "username"
-QT_MOC_LITERAL(19, 287, 8) // "userLeft"
+QT_MOC_LITERAL(13, 197, 8), // "sendTurn"
+QT_MOC_LITERAL(14, 206, 10), // "sendPoints"
+QT_MOC_LITERAL(15, 217, 22), // "disconnectedFromServer"
+QT_MOC_LITERAL(16, 240, 5), // "error"
+QT_MOC_LITERAL(17, 246, 28), // "QAbstractSocket::SocketError"
+QT_MOC_LITERAL(18, 275, 11), // "socketError"
+QT_MOC_LITERAL(19, 287, 10), // "userJoined"
+QT_MOC_LITERAL(20, 298, 8), // "username"
+QT_MOC_LITERAL(21, 307, 8) // "userLeft"
 
     },
     "Game\0on_pushButton_clicked\0\0"
@@ -59,7 +61,8 @@ QT_MOC_LITERAL(19, 287, 8) // "userLeft"
     "scrollToHCenter\0scrollToVCenter\0"
     "on_pushButton_7_clicked\0"
     "on_einstellungen_clicked\0messageReceived\0"
-    "sender\0text\0sendMessage\0disconnectedFromServer\0"
+    "sender\0text\0sendMessage\0sendTurn\0"
+    "sendPoints\0disconnectedFromServer\0"
     "error\0QAbstractSocket::SocketError\0"
     "socketError\0userJoined\0username\0"
     "userLeft"
@@ -72,7 +75,7 @@ static const uint qt_meta_data_Game[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      13,   14, // methods
+      15,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -80,19 +83,21 @@ static const uint qt_meta_data_Game[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   79,    2, 0x08 /* Private */,
-       3,    0,   80,    2, 0x08 /* Private */,
-       4,    0,   81,    2, 0x08 /* Private */,
-       5,    2,   82,    2, 0x08 /* Private */,
-       6,    2,   87,    2, 0x08 /* Private */,
-       7,    0,   92,    2, 0x08 /* Private */,
-       8,    0,   93,    2, 0x08 /* Private */,
-       9,    2,   94,    2, 0x08 /* Private */,
-      12,    0,   99,    2, 0x08 /* Private */,
-      13,    0,  100,    2, 0x08 /* Private */,
-      14,    1,  101,    2, 0x08 /* Private */,
-      17,    1,  104,    2, 0x08 /* Private */,
-      19,    1,  107,    2, 0x08 /* Private */,
+       1,    0,   89,    2, 0x08 /* Private */,
+       3,    0,   90,    2, 0x08 /* Private */,
+       4,    0,   91,    2, 0x08 /* Private */,
+       5,    2,   92,    2, 0x08 /* Private */,
+       6,    2,   97,    2, 0x08 /* Private */,
+       7,    0,  102,    2, 0x08 /* Private */,
+       8,    0,  103,    2, 0x08 /* Private */,
+       9,    2,  104,    2, 0x08 /* Private */,
+      12,    0,  109,    2, 0x08 /* Private */,
+      13,    0,  110,    2, 0x08 /* Private */,
+      14,    0,  111,    2, 0x08 /* Private */,
+      15,    0,  112,    2, 0x08 /* Private */,
+      16,    1,  113,    2, 0x08 /* Private */,
+      19,    1,  116,    2, 0x08 /* Private */,
+      21,    1,  119,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -105,9 +110,11 @@ static const uint qt_meta_data_Game[] = {
     QMetaType::Void, QMetaType::QString, QMetaType::QString,   10,   11,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, 0x80000000 | 15,   16,
-    QMetaType::Void, QMetaType::QString,   18,
-    QMetaType::Void, QMetaType::QString,   18,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 17,   18,
+    QMetaType::Void, QMetaType::QString,   20,
+    QMetaType::Void, QMetaType::QString,   20,
 
        0        // eod
 };
@@ -127,16 +134,18 @@ void Game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         case 6: _t->on_einstellungen_clicked(); break;
         case 7: _t->messageReceived((*reinterpret_cast< const QString(*)>(_a[1])),(*reinterpret_cast< const QString(*)>(_a[2]))); break;
         case 8: _t->sendMessage(); break;
-        case 9: _t->disconnectedFromServer(); break;
-        case 10: _t->error((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
-        case 11: _t->userJoined((*reinterpret_cast< const QString(*)>(_a[1]))); break;
-        case 12: _t->userLeft((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        //case 9: _t->sendTurn(); break;
+        //case 10: _t->sendPoints(); break;
+        case 11: _t->disconnectedFromServer(); break;
+        case 12: _t->error((*reinterpret_cast< QAbstractSocket::SocketError(*)>(_a[1]))); break;
+        case 13: _t->userJoined((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 14: _t->userLeft((*reinterpret_cast< const QString(*)>(_a[1]))); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
         switch (_id) {
         default: *reinterpret_cast<int*>(_a[0]) = -1; break;
-        case 10:
+        case 12:
             switch (*reinterpret_cast<int*>(_a[1])) {
             default: *reinterpret_cast<int*>(_a[0]) = -1; break;
             case 0:
@@ -176,13 +185,13 @@ int Game::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 13)
+        if (_id < 15)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 13;
+        _id -= 15;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 13)
+        if (_id < 15)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 13;
+        _id -= 15;
     }
     return _id;
 }
